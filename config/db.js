e592d694f89a2e7e87945d1b76d81f19017d1dfa
Shiv Srivastava <1983.shiv@gmail.com>
2023-01-27 07:29:40 +0530

    prepare basic structure for crud operation

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// const mongoose = require("mongoose");
+import { mongoose } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connnect : ${conn.connection.host}`);
+  } catch (err) {
+    console.log("some error occured", err.message);
+  }
+};
+
+export default connectDB;
+// module.exports = connectDB;
