4b405cba9aeae05961ecf9366ca8017785693f55
Shiv Srivastava <1983.shiv@gmail.com>
2023-01-27 23:41:33 +0530

    completed add/edit/delete operation for stories

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const connectDB = require("./config/db");
 const morgan = require("morgan");
 const { engine } = require("./node_modules/express-handlebars/dist/index");
 const dotenv = require("dotenv");
+const methodOverride = require("method-override");
 const routes = require("./routes/index");
 const passport = require("passport");
-const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 
@@ -24,13 +24,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 // date format
-const { formatDate, stripTags, truncate } = require("./helpers/hbs");
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
 
 // Handlebars
 // app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
@@ -39,7 +56,7 @@ const { formatDate, stripTags, truncate } = require("./helpers/hbs");
 app.engine(
   ".hbs",
   engine({
-    helpers: { formatDate, stripTags, truncate },
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: "main",
     extname: ".hbs",
   })
@@ -64,6 +81,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // static folder
 app.use(express.static(path.join(__dirname, "public")));
--- routes/stories.js
+++ routes/stories.js
@@ -29,11 +29,65 @@ router.get("/add", ensureAuth, (req, res) => {
 });
 
 // @desc  Process the add form
-// @route GET  /stories/add
+// @route POST  /stories/add
 router.post("/add", ensureAuth, async (req, res) => {
   try {
     req.body.user = req.user.id;
     await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.warn(error);
+    res.render("error/500");
+  }
+});
+
+// @desc  Stories edit page
+// @route GET  /stories/edit
+
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  const story = await Story.findOne({ _id: req.params.id }).lean();
+
+  if (!story) {
+    return res.render("error/404");
+  }
+  if (story.user != req.user.id) {
+    return res.render("/stories");
+  } else {
+    res.render("stories/edit", {
+      story,
+    });
+  }
+});
+
+// @desc    Process the edit form
+// @route   PUT  /stories/edit/:id
+router.put("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.redirect("/dashboard");
+    }
+  } catch (error) {
+    console.warn(error);
+    res.render("error/500");
+  }
+});
+
+// @desc    Delete a story form
+// @route   DELETE  /stories/delete/:id
+router.delete("/delete/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
   } catch (error) {
     console.warn(error);
